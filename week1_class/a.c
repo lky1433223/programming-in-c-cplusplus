071add2576a35a51b2608f24d6c7fe06dab1f7ce
ShiinaRikka <541665621@qq.com>
2022-02-22 17:21:21 +0800

    添加项目文件。

---
+++ week1_class/a.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("Input a and b\n");
+	scanf_s("%d %d", &a, &b);
+	printf("a + b = %d", c = a + b);
+}
---
+++ week1_class/a.cpp
@@ -0,0 +1,9 @@
+#include<cstdio>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	scanf_s("%d %d", &a, &b);
+	c = a + b;
+	printf("%d", c);
+}
